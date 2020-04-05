26d0d86145bb68524098f3aef0105f9024ad8c42
Jeremy Long <jeremy.long@gmail.com>
2020-04-05 06:53:41 -0400

    updated version and added build scripts

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+dotnet clean
+echo "attempting to upgrade dependencies"
+pushd GrokAssembly
+dotnet add package System.Reflection.Metadata
+popd
+echo "build started"
+dotnet build
+dotnet publish
+echo "build complete"
+
+dotnet ./GrokAssembly/bin/Debug/netcoreapp3.1/GrokAssembly.dll ./GrokAssembly/bin/Debug/netcoreapp3.1/GrokAssembly.dll
+if [[ "$?" -eq 0 ]] ; then
+  echo 
+  echo 
+  echo "SUCCESS - dependency-check docker test passed"
+else
+  echo 
+  echo 
+  echo "FAILED - dependency-check docker test failed"
+  exit 1
+fi
---
+++ release.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+PUBLISH=./GrokAssembly/bin/Release/netcoreapp3.1/publish
+TEMP=./GrokAssembly/bin/Release/netcoreapp3.1/temp
+
+echo "clean"
+dotnet clean --configuration Release
+echo "build"
+dotnet build --configuration Release
+echo "publish"
+dotnet publish --configuration Release
+echo "package"
+
+dotnet ./GrokAssembly/bin/Release/netcoreapp3.1/publish/GrokAssembly.dll ./GrokAssembly/bin/Release/netcoreapp3.1/publish/GrokAssembly.dll
+
+if [[ "$?" -eq 0 ]] ; then
+  echo "SUCCESS - GrokAssembly test passed"
+else
+  echo "FAILED - GrokAssembly test failed"
+  exit 1
+fi
+
+
+
+##[[ -d "$TEMP" ]] || mkdir "$TEMP"
+##rm -rfv "$TEMP/*"
+##cp -R "$PUBLISH" "$TEMP"/GrokAssembly
+##pushd "$TEMP"
+pushd "$PUBLISH"
+zip -r GrokAssembly.zip ./ -x "*.DS_Store"
+popd
+##cp "$PUBLISH"/GrokAssembly.zip "$PUBLISH"
+##rm -rfv "$TEMP"
+
+echo "Packaging complete: "$PUBLISH"/GrokAssembly.zip"
